2a86dc27935b67cda06587a80c7d87efafcd01b2
Benjamin Beisheim <beisheim.b@gmail.com>
2025-01-29 18:30:10 -0600

    first commit

---
+++ src/main/java/com/quantitative/options_pricing_service/OptionsPricingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.quantitative.options_pricing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionsPricingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionsPricingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantitative/options_pricing_service/OptionsPricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantitative.options_pricing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionsPricingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
